49abd2aa0b64cd1f13ff767f72d40717e7d95b0d
Biokes <106444765+Biokes@users.noreply.github.com>
2025-09-30 12:21:36 +0100

    added Diamond

--- src/erc20Diamond/Diamond.sol
+++ src/erc20Diamond/Diamond.sol
@@ -5,6 +5,9 @@ import {DiamondLibrary} from "./libs/DiamondLibrary.sol";
 
 contract ERC20Diamond{
     DiamondLibrary.DiamondStorage public token;
-    mapping (bytes4=> address) facetsAddress;
+    mapping (bytes4=> address) facetsToAddress;
     
+
+    fallback() payable external {}
+    receive() payable external {}
 }
---
+++ test/diamond/Diamond.t.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/test.sol";
+
+contract DiamondTest is Test{
+    
+}
