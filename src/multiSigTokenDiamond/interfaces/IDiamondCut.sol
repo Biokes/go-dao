e9bcfdfddc0c5c727533b7df1c30c1e5151cf77e
Biokes <106444765+Biokes@users.noreply.github.com>
2025-10-01 12:11:42 +0100

    added Interface Diamond

---
+++ src/multiSigTokenDiamond/interfaces/IDiamondCut.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+interface IDiamondCut {
+    enum FacetCutAction {
+        Add,
+        Replace,
+        Remove
+    }
+    
+    struct FacetCut {
+        address facetAddress;
+        FacetCutAction action;
+        bytes4[] functionSelectors;
+    }
+
+    event DiamondCut(FacetCut[] _diamondCut, address _init, bytes _calldata);
+}
