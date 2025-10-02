0fba86bc4c626e13a8d1ea410644ddb2c7bf2777
Biokes <106444765+Biokes@users.noreply.github.com>
2025-10-02 15:51:25 +0100

    refactor diamond costructor to add facet at deployment

--- src/multiSigTokenDiamond/Diamond.sol
+++ src/multiSigTokenDiamond/Diamond.sol
@@ -1,10 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 import {MultiSigTokenUtils} from "./libs/multiSigTokenLibrary.sol";
+import {IDiamondLoupe} from "../multiSigTokenDiamond/interfaces/IDiamondLoupe.sol";
+import {IDiamondCut} from "../multiSigTokenDiamond/interfaces/IDiamondCut.sol";
+import {IDiamond} from "../multiSigTokenDiamond/interfaces/IDiamond.sol";
+
 
 contract Diamond{
     using MultiSigTokenUtils for MultiSigTokenUtils.DiamondStorage;
 
+    constructor(address diamondFacetCut){
+        MultiSigTokenUtils.DiamondStorage storage ds = MultiSigTokenUtils.getDiamondStorage();
+        ds._owner = msg.sender;
+        bytes4[] memory selectors = new bytes4[](6);
+        selectors[0] = IDiamondCut.facetCut.selector;
+        selectors[1] = IDiamondLoupe.facets.selector;
+        selectors[2] = IDiamondLoupe.facetFunctionSelectors.selector;
+        selectors[3] = IDiamondLoupe.facetAddresses.selector;
+        selectors[4] = IDiamondLoupe.facetAddress.selector;
+        selectors[5] = IDiamondLoupe.supportsInterface.selector;
+        IDiamondCut.FacetCut[] memory facet = new IDiamondCut.FacetCut[](1);
+        facet[0] = IDiamond.FacetCut({
+            facetAddress: diamondFacetCut,
+            action: IDiamond.FacetCutAction.Add,
+            functionSelectors: selectors
+        });
+        MultiSigTokenUtils.diamondCut(facet, diamondFacetCut,"");
+    }
     receive() external payable {}
 
     fallback() external payable {
